2cf499cd89bd7a35f0131a762b021a69f73fe712
Diego Forero <ds.forero@hotmail.com>
2024-09-09 07:52:04 -0500

    Primer avance

---
+++ app.js
@@ -0,0 +1,27 @@
+console.log("Hola mundo");
+
+let numero1 = 35;
+let numero2 = 45;
+
+let pi = 3.1416;
+
+let estado = true;
+
+let direccion = "Calle con Carrera"
+
+console.log(numero1);
+console.log(numero2);
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+
+console.log(suma);
+console.log(direccion);
+
+console.log('Este es el primer valor ' + numero1 + ' el segundo valor es ' + numero2 + ' y la suma de estos dos valores es ' + suma);
+
+//Literal Template
+console.log(`El primer valor es ${numero1} y el segundo valor es ${numero2} y la suma de estos dos numeros es ${suma}`);
+console.log(`El primer valor es ${numero1} y el segundo valor es ${numero2} y la multipicacion de estos dos numeros es ${multiplicacion}`);
+console.log(`El primer valor es ${numero1} y el segundo valor es ${numero2} y la resta de estos dos numeros es ${resta}`);
